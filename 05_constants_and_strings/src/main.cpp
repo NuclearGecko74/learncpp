3b7a25440027e6ec936039a3b0136535f9bfe185
Anton Olguin <antonolguin1@gmail.com>
2026-06-07 06:40:33 -0600

    strings challenge

--- 05_constants_and_strings/src/main.cpp
+++ 05_constants_and_strings/src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello from 05_constants_and_strings!" << std::endl;
-    return 0;
+std::string getName()
+{
+    std::string fullName {};
+
+    std::cout << "Enter your full name: ";
+    std::getline(std::cin >> std::ws, fullName);
+
+    return fullName;
+}
+
+int getAge()
+{
+    int age{};
+
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+
+    return age;
+}
+
+int main()
+{
+    std::string name { getName() };
+    int age { getAge() };
+
+    int nameLength { static_cast<int>(name.length()) };
+
+    std::cout << "Your age + length of name is: " << age + nameLength << '\n'; 
+
+    return EXIT_SUCCESS;
 }
