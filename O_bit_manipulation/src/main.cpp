300a8625e84ce7b32b1b65d6e415ed91974e78ac
Anton Olguin <antonolguin1@gmail.com>
2026-06-10 03:49:35 -0600

    O.3 challenge bitwase operators

--- O_bit_manipulation/src/main.cpp
+++ O_bit_manipulation/src/main.cpp
@@ -1,6 +1,27 @@
+/*
+A bitwise rotation is like a bitwise shift, except that any bits
+shifted off one end are added back to the other end. For example
+0b1001 << 1 would be 0b0010, but a left rotate by 1 would result in 
+0b0011 instead. Implement a function that does a left rotate on a 
+std::bitset<4>. For this one, it’s okay to use test() and set().
+*/
+
+#include <bitset>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello from O_bit_manipulation!" << std::endl;
-    return 0;
+// "rotl" stands for "rotate left"
+std::bitset<4> rotl(std::bitset<4> bits)
+{
+    return (bits << 1) | (bits >> 3);
+}
+
+int main()
+{
+	std::bitset<4> bits1{ 0b0001 };
+	std::cout << rotl(bits1) << '\n';
+
+	std::bitset<4> bits2{ 0b1001 };
+	std::cout << rotl(bits2) << '\n';
+
+	return 0;
 }
