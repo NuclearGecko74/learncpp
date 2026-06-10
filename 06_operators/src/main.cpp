9792f7f36da382a094fb5f9db5b70fb1c69d3c54
Anton Olguin <antonolguin1@gmail.com>
2026-06-09 19:38:27 -0600

    6.3 challenge

--- 06_operators/src/main.cpp
+++ 06_operators/src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello from 06_operators!" << std::endl;
-    return 0;
+int getInt()
+{
+    std::cout << "Enter an integer: ";
+    int x{};
+    std::cin >> x;
+    return x;
+}
+
+constexpr bool isEven(int x)
+{
+    return x % 2 == 0;
+}
+
+int main()
+{
+	int x { getInt() };
+
+    if (isEven(x))
+        std::cout << x << " is even\n";
+    else
+        std::cout << x << " is odd\n";
+
+    return EXIT_SUCCESS;
 }
