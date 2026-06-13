58738bfaaeccdccb62c952a0cc6e2b4fe140c0c2
Anton Olguin <antonolguin1@gmail.com>
2026-06-13 04:33:10 -0600

    section 8 challenge 1

--- 08_control_flow/src/main.cpp
+++ 08_control_flow/src/main.cpp
@@ -1,24 +1,62 @@
 #include <iostream>
 
-void fizzbuzz(int x)
+namespace Constants
 {
-    for (int i { 1 }; i <= x; i++)
-    {
-        if (i % 3 == 0)
-            std::cout << "fizz";
-        if (i % 5 == 0)
-            std::cout << "buzz";
-        if (i % 7 == 0)
-            std::cout << "pop";
-        if ((i % 3) && (i % 5) && (i % 7))
-            std::cout << i;
-        std::cout << '\n';
-    }
+	constexpr double gravity { 9.8 };
+}
+
+// Gets tower height from user and returns it
+double getTowerHeight()
+{
+	std::cout << "Enter the height of the tower in meters: ";
+	double towerHeight{};
+	std::cin >> towerHeight;
+	return towerHeight;
+}
+
+// Returns the current ball height after "seconds" seconds
+// note: this function could be constexpr, but we haven't covered constexpr functions yet
+double calculateBallHeight(double towerHeight, int seconds)
+{
+	// Using formula: s = (u * t) + (a * t^2) / 2
+	// here u (initial velocity) = 0, so (u * t) = 0
+	const double fallDistance { Constants::gravity * (seconds * seconds) / 2.0 };
+	const double ballHeight { towerHeight - fallDistance };
+
+	if (ballHeight < 0.0)
+		return 0.0;
+
+	return ballHeight;
+}
+
+// Prints ball height above ground
+void printBallHeight(double ballHeight, int seconds)
+{
+	if (ballHeight > 0.0)
+		std::cout << "At " << seconds << " seconds, the ball is at height: " << ballHeight << " meters\n";
+	else
+		std::cout << "At " << seconds << " seconds, the ball is on the ground.\n";
+}
+
+// Calculates the current ball height and then prints it
+// This is a helper function to make it easier to do this
+double calculateAndPrintBallHeight(double towerHeight, int seconds)
+{
+	const double ballHeight{ calculateBallHeight(towerHeight, seconds) };
+	printBallHeight(ballHeight, seconds);
+
+	return ballHeight;
 }
 
 int main()
 {
-    fizzbuzz(150);
-    
-    return EXIT_SUCCESS;
+	const double towerHeight{ getTowerHeight() };
+
+	int seconds { 0 };
+	while (calculateAndPrintBallHeight(towerHeight, seconds) > 0.0)
+	{
+		++seconds;
+	}
+
+	return 0;
 }
