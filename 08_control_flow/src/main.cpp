89a5912ec12b9d1a3318c8c587796231897f37d1
Anton Olguin <antonolguin1@gmail.com>
2026-06-13 02:33:23 -0600

    print a through z

--- 08_control_flow/src/main.cpp
+++ 08_control_flow/src/main.cpp
@@ -1,47 +1,16 @@
 #include <iostream>
 
-int calculate(int x, int y, char op)
-{
-    switch (op)
-    {
-    case '+': return x + y;
-    case '-': return x - y;
-    case '*': return x * y;
-    case '/': return x / y;
-    case '%': return x % y;
-    default:
-        std::cerr << "calculate(): Invalid Operator!\n";
-        return 0;
-    }
-}
-
-int getInt()
-{
-    int x{};
-    std::cin >> x;
-    return x;
-}
-
-char getChar()
-{
-    char x{};
-    std::cin >> x;
-    return x;
-}
+// Write a program that prints out the letters a through z along with their ASCII codes. Use a loop variable of type char.
 
 int main()
 {
-    std::cout << "Enter an integer: ";
-    int num1 { getInt() };
+    char ch { 'a' };
+    while (ch <= 'z')
+    {
+        std::cout << ch << ": " << static_cast<int>(ch) << '\n';
+        ch++;
+    }
+    
 
-    std::cout << "Enter another integer: ";
-    int num2 { getInt() };
-
-    std::cout << "Enter operation (+,-,*,/,%): ";
-    char op{ getChar() };
-
-    int result { calculate(num1, num2, op) };
-    std::cout << num1 << ' ' << op << ' ' << num2 << " = " << result << '\n';
-
-    return EXIT_SUCCESS;
+    return 0;
 }
