433f7ad6d1d81c64c6c305da61faecad83306abd
Anton Olguin <antonolguin1@gmail.com>
2026-06-13 04:42:28 -0600

    prime numbers challenge

--- 08_control_flow/src/main.cpp
+++ 08_control_flow/src/main.cpp
@@ -1,62 +1,45 @@
+// Make sure that assert triggers even if we compile in release mode
+#undef NDEBUG
+
+#include <cassert> // for assert
 #include <iostream>
+#include <math.h>
 
-namespace Constants
+bool isPrime(int x)
 {
-	constexpr double gravity { 9.8 };
-}
+    if (x <= 1) return false;
+    if (x == 2) return true;
+    if (x % 2 == 0) return false;
 
-// Gets tower height from user and returns it
-double getTowerHeight()
-{
-	std::cout << "Enter the height of the tower in meters: ";
-	double towerHeight{};
-	std::cin >> towerHeight;
-	return towerHeight;
-}
-
-// Returns the current ball height after "seconds" seconds
-// note: this function could be constexpr, but we haven't covered constexpr functions yet
-double calculateBallHeight(double towerHeight, int seconds)
-{
-	// Using formula: s = (u * t) + (a * t^2) / 2
-	// here u (initial velocity) = 0, so (u * t) = 0
-	const double fallDistance { Constants::gravity * (seconds * seconds) / 2.0 };
-	const double ballHeight { towerHeight - fallDistance };
-
-	if (ballHeight < 0.0)
-		return 0.0;
-
-	return ballHeight;
-}
-
-// Prints ball height above ground
-void printBallHeight(double ballHeight, int seconds)
-{
-	if (ballHeight > 0.0)
-		std::cout << "At " << seconds << " seconds, the ball is at height: " << ballHeight << " meters\n";
-	else
-		std::cout << "At " << seconds << " seconds, the ball is on the ground.\n";
-}
-
-// Calculates the current ball height and then prints it
-// This is a helper function to make it easier to do this
-double calculateAndPrintBallHeight(double towerHeight, int seconds)
-{
-	const double ballHeight{ calculateBallHeight(towerHeight, seconds) };
-	printBallHeight(ballHeight, seconds);
-
-	return ballHeight;
+    for (int i{2}; i <= static_cast<int>(std::sqrt(x)); i++)
+    {
+        if (x % i == 0)
+            return false;
+    }
+    return true;
 }
 
 int main()
 {
-	const double towerHeight{ getTowerHeight() };
+    assert(!isPrime(0)); // terminate program if isPrime(0) is true
+    assert(!isPrime(1));
+    assert(isPrime(2));  // terminate program if isPrime(2) is false
+    assert(isPrime(3));
+    assert(!isPrime(4));
+    assert(isPrime(5));
+    assert(isPrime(7));
+    assert(!isPrime(9));
+    assert(isPrime(11));
+    assert(isPrime(13));
+    assert(!isPrime(15));
+    assert(!isPrime(16));
+    assert(isPrime(17));
+    assert(isPrime(19));
+    assert(isPrime(97));
+    assert(!isPrime(99));
+    assert(isPrime(13417));
 
-	int seconds { 0 };
-	while (calculateAndPrintBallHeight(towerHeight, seconds) > 0.0)
-	{
-		++seconds;
-	}
+    std::cout << "Success!\n";
 
-	return 0;
+    return 0;
 }
