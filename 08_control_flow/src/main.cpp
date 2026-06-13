21836d1968e1a515fd8237fd8f952360b8710a52
Anton Olguin <antonolguin1@gmail.com>
2026-06-13 02:00:04 -0600

    8.6 switch statements challenge

--- 08_control_flow/src/main.cpp
+++ 08_control_flow/src/main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello from 08_control_flow!" << std::endl;
-    return 0;
+int calculate(int x, int y, char op)
+{
+    switch (op)
+    {
+    case '+': return x + y;
+    case '-': return x - y;
+    case '*': return x * y;
+    case '/': return x / y;
+    case '%': return x % y;
+    default:
+        std::cerr << "calculate(): Invalid Operator!\n";
+        return 0;
+    }
+}
+
+int getInt()
+{
+    int x{};
+    std::cin >> x;
+    return x;
+}
+
+char getChar()
+{
+    char x{};
+    std::cin >> x;
+    return x;
+}
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int num1 { getInt() };
+
+    std::cout << "Enter another integer: ";
+    int num2 { getInt() };
+
+    std::cout << "Enter operation (+,-,*,/,%): ";
+    char op{ getChar() };
+
+    int result { calculate(num1, num2, op) };
+    std::cout << num1 << ' ' << op << ' ' << num2 << " = " << result << '\n';
+
+    return EXIT_SUCCESS;
 }
