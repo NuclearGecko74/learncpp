4826b08255aef9e6176e92f981b17d0f75a466de
Anton Olguin <antonolguin1@gmail.com>
2026-06-15 02:28:56 -0600

    upgrade calculator to support unexpected input

--- 09_error_handling/src/main.cpp
+++ 09_error_handling/src/main.cpp
@@ -1,6 +1,94 @@
 #include <iostream>
+#include <limits>
 
-int main() {
-    std::cout << "Hello from 09_error_handling!" << std::endl;
+void ignoreLine()
+{
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+bool clearFailedExtraction()
+{
+    if (std::cin.fail())
+    {
+        if (std::cin.eof())
+        {
+            std::exit(0);
+        }
+        std::cin.clear();
+        ignoreLine();
+        return true;
+    }
+    return false;
+}
+
+double getDouble()
+{
+    while (true)
+    {
+        std::cout << "Enter a decimal number: ";
+        double x{};
+        std::cin >> x;
+
+        if (clearFailedExtraction())
+        {
+            std::cout << "Error: Invalid Input. Please Try again.\n";
+            continue;
+        }
+        ignoreLine();
+        return x;
+    }
+}
+
+char getOperator()
+{
+    while (true)
+    {
+        std::cout << "Enter one of the following: +, -, *, or /: ";
+        char op{};
+        std::cin >> op;
+
+        switch (op)
+        {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return op;
+        default:
+            std::cout << "Oops, that input is invalid.  Please try again.\n";
+        }
+    }
+    
+}
+
+void printResult(double x, char operation, double y)
+{
+    std::cout << x << ' ' << operation << ' ' << y << " is ";
+
+    switch (operation)
+    {
+    case '+':
+        std::cout << x + y << '\n';
+        return;
+    case '-':
+        std::cout << x - y << '\n';
+        return;
+    case '*':
+        std::cout << x * y << '\n';
+        return;
+    case '/':
+        std::cout << x / y << '\n';
+        return;
+    }
+}
+
+int main()
+{
+    double x{ getDouble() };
+    char operation{ getOperator() };
+    double y{ getDouble() };
+
+    printResult(x, operation, y);
+
     return 0;
 }
